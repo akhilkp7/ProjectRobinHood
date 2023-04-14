84fc443376999d28d259ae9937ff1c55f8af19b7
Akhil <akhilpalla@vt.edu>
2023-04-14 15:03:39 -0400

    Added code to transform import statement

---
+++ build.sbt
@@ -0,0 +1,20 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.13.10"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "ProjectRobinHood"
+  )
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.3.2"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.3.2"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-hive
+libraryDependencies += "org.apache.spark" %% "spark-hive" % "3.3.2"
+
+// build.sbt
+libraryDependencies += "org.scalameta" %% "scalameta" % "4.7.6"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,37 @@
+import scala.meta._
+object Main {
+  def main(args: Array[String]): Unit = {
+
+    val count = new WordCount();
+    count.countWords();
+
+    val path = java.nio.file.Paths.get("C:\\Users\\Akhil\\IdeaProjects\\ProjectRobinHood\\src\\main\\scala\\WordCount.scala")
+    val bytes = java.nio.file.Files.readAllBytes(path)
+    val text = new String(bytes, "UTF-8")
+    val input = Input.VirtualFile(path.toString, text)
+    val exampleTree = input.parse[Source].get
+
+//    print(exampleTree.syntax)
+    print(exampleTree.structure)
+    println()
+    object ImportTransformer extends Transformer {
+      override def apply(tree: Tree): Tree = tree match {
+        // Match on Import nodes
+        case i @ Importer(q"org.apache.spark", importees) =>
+          // Replace the SparkContext importee with SparkStreaming
+          val newImportees = importees.map {
+            case i @ Importee.Name(Name("SparkContext")) =>
+              i.copy(name = Name("SparkStreaming"))
+            case other => other
+          }
+          i.copy(importees = newImportees)
+
+        // Recursively traverse the AST
+        case other => super.apply(other)
+      }
+    }
+
+    println(ImportTransformer(exampleTree))
+
+  }
+}
