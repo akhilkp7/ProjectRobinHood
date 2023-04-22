fa71976b5367c52e3deb08eca2f9652b10e6b257
Ramnath Raghu <ramnathraghu@Ramnaths-MacBook-Air.local>
2023-04-22 15:08:46 -0400

    Changes to Main Class

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,10 @@
+import org.apache.spark.sql.catalyst.ScalaReflection.universe.EmptyTree
+
 import scala.meta._
+import java.nio.file._
+import java.nio.charset.StandardCharsets
+import java.io._
+import java._
 object Main {
   def main(args: Array[String]): Unit = {
 
@@ -10,28 +16,44 @@ object Main {
     val text = new String(bytes, "UTF-8")
     val input = Input.VirtualFile(path.toString, text)
     val exampleTree = input.parse[Source].get
-
+    val outputFile = new File("src/main/scala/WordCount_Transformed.scala")
+    val writer = new PrintWriter(outputFile)
 //    print(exampleTree.syntax)
     print(exampleTree.structure)
     println()
+
+    val one= q"val a=10"
+    val two =q"val b=20"
     object ImportTransformer extends Transformer {
       override def apply(tree: Tree): Tree = tree match {
         // Match on Import nodes
-        case i @ Importer(q"org.apache.spark", importees) =>
+        case i@Importer(q"org.apache.spark", importees) =>
           // Replace the SparkContext importee with SparkStreaming
           val newImportees = importees.map {
-            case i @ Importee.Name(Name("SparkContext")) =>
-              i.copy(name = Name("SparkStreaming"))
+            case i@Importee.Name(Name("SparkContext")) =>
+              i.copy(name = Name("SparkContext"))
             case other => other
           }
           i.copy(importees = newImportees)
-
-        // Recursively traverse the AST
+        case q"val $name = new SparkContext($conf)" =>
+          q"val sparkConf = new SparkConf().setAppName(${Lit.String("StatefulNetworkWordCount")})"
+        case q"val $pat = $expr" if pat.syntax == "textFile" && expr.syntax.contains(".textFile(") =>
+          q"""
+                      val ssc = new StreamingContext(sparkConf, Seconds(1))
+                      ssc.checkpoint(".")
+                    """
         case other => super.apply(other)
       }
     }
 
+
+
     println(ImportTransformer(exampleTree))
+    /*    val transformedTree = ImportTransformer(exampleTree)
+        writer.write(transformedTree.syntax)
+        writer.close()
+
+        println(s"Transformed code written to: ${outputFile.getAbsolutePath}")*/
 
   }
 }
